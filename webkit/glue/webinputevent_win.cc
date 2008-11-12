wistoch/meego-app-browser

commit bf6044be4bea93cb321334a015b191cd9d06718c

Implements a WM_MOUSEHWHEEL message handler. A change r2266 removed fake scroll bars used to handle WM_MOUSEWHEEL messages. This change prevents Windows from sending WM_HSCROLL messages when tilting a mouse wheel (Windows sends WM_HSCROLL messages only to a window which has scroll bars.) and this causes a regression problem that we cannot use tilt-wheel to scroll horizontally. To solve this problem, this change implements a WM_MOUSEHWHEEL (0x020E) message handler to dispatch tilt-wheel events to the RenderWidgetHostViewWin::OnWheelEvents() function. Also, It changes scroll direction in creating a WebMouseWheelEvent instance. Review URL: http://codereview.chromium.org/9319
git-svn-id: http://src.chromium.org/svn/trunk/src@5095 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

webkit/glue/webinputevent_win.cc
@@ -172,22 +172,14 @@ WebMouseWheelEvent::WebMouseWheelEvent(HWND hwnd, UINT message, WPARAM wparam,
         break;
     }
 
-    // Windows sends the following messages for tilt-wheel events.
-    //  * Tilt a mousewheel (left)
-    //    message == WM_HSCROLL, wparam == SB_LINELEFT (== SB_LINEUP).
-    //  * Tilt a mousewheel (right)
-    //    message == WM_HSCROLL, wparam == SB_LINERIGHT (== SB_LINEDOWN).
-    // To convert these events to the shift + mousewheel ones, we do not only
-    // add a shift but also change the signs of their |wheel_delta| values.
-    if (WM_HSCROLL == message) {
-      key_state |= MK_SHIFT;
-      wheel_delta = -wheel_delta;
-    }
-
     // Use GetAsyncKeyState for key state since we are synthesizing 
     // the input
     get_key_state = GetAsyncKeyState;
   } else {
+    // TODO(hbono): we should add a new variable which indicates scroll
+    // direction and remove this key_state hack.
+    if (WM_MOUSEHWHEEL == message)
+      key_state |= MK_SHIFT;
 
     global_x = static_cast<short>(LOWORD(lparam));
     global_y = static_cast<short>(HIWORD(lparam));

chrome/browser/render_widget_host_view_win.h
@@ -109,6 +109,7 @@ class RenderWidgetHostViewWin :
     MESSAGE_HANDLER(WM_KEYDOWN, OnKeyEvent)
     MESSAGE_HANDLER(WM_KEYUP, OnKeyEvent)
     MESSAGE_HANDLER(WM_MOUSEWHEEL, OnWheelEvent)
+    MESSAGE_HANDLER(WM_MOUSEHWHEEL, OnWheelEvent)
     MESSAGE_HANDLER(WM_HSCROLL, OnWheelEvent)
     MESSAGE_HANDLER(WM_VSCROLL, OnWheelEvent)
     MESSAGE_HANDLER(WM_CHAR, OnKeyEvent)

commit 0734a5cfa422d5fb3922ec24f331d7a87b5ff767

Fixes a regression caused by my change r5095. This change fixes a regression problem caused by my change r5095. ThinkPad touchpads (or trackpoints) send WM_HSCROLL messages and we still need a hack for WM_HSCROLL messages. Review URL: http://codereview.chromium.org/10295
git-svn-id: http://src.chromium.org/svn/trunk/src@5254 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

webkit/glue/webinputevent_win.cc
@@ -171,6 +171,17 @@ WebMouseWheelEvent::WebMouseWheelEvent(HWND hwnd, UINT message, WPARAM wparam,
         break;
     }
 
+    // Touchpads (or trackpoints) send the following messages in scrolling
+    // horizontally.
+    //  * Scrolling left
+    //    message == WM_HSCROLL, wparam == SB_LINELEFT (== SB_LINEUP).
+    //  * Scrolling right
+    //    message == WM_HSCROLL, wparam == SB_LINERIGHT (== SB_LINEDOWN).
+    if (WM_HSCROLL == message) {	
+      key_state |= MK_SHIFT;	
+      wheel_delta = -wheel_delta;	
+    }
+
     // Use GetAsyncKeyState for key state since we are synthesizing 
     // the input
     get_key_state = GetAsyncKeyState;
wistoch/meego-app-browser

commit 015ff4a1710fdbb779786b611d45ae8b46241ecd

Connect a menu to the "Other bookmarks" button.
Review URL: http://codereview.chromium.org/113104

git-svn-id: http://src.chromium.org/svn/trunk/src@15560 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

chrome/browser/bookmarks/bookmark_menu_controller_gtk.cc
@@ -27,12 +27,6 @@ BookmarkMenuController::BookmarkMenuController(Browser* browser,
   menu_.reset(new MenuGtk(this, false));
   int next_menu_id = 1;
   BuildMenu(node, start_child_index, menu_.get(), &next_menu_id);
-
-  // TODO(erg): Add the BuildOtherFolderMenu() to handle the overflow case from
-  // the chevron on the bookmark bar.
-  //
-  //  if (show_other_folder)
-  //    BuildOtherFolderMenu(&next_menu_id);
 }
 
 BookmarkMenuController::~BookmarkMenuController() {

chrome/browser/gtk/bookmark_bar_gtk.cc
@@ -126,8 +126,7 @@ void BookmarkBarGtk::Init(Profile* profile) {
                      FALSE, FALSE, 0);
 
   other_bookmarks_button_ = gtk_chrome_button_new();
-  g_signal_connect(other_bookmarks_button_, "button-press-event",
-                   G_CALLBACK(&OnButtonPressed), this);
+  ConnectFolderButtonEvents(other_bookmarks_button_);
   gtk_button_set_label(
       GTK_BUTTON(other_bookmarks_button_),
       l10n_util::GetStringUTF8(IDS_BOOMARK_BAR_OTHER_BOOKMARKED).c_str());
@@ -326,28 +325,8 @@ GtkWidget* BookmarkBarGtk::CreateBookmarkButton(
   g_signal_connect(G_OBJECT(button), "drag-end",
                    G_CALLBACK(&OnButtonDragEnd), this);
 
-
-  if (node->is_url()) {
-    // Connect to 'button-release-event' instead of 'clicked' because we need
-    // access to the modifier keys and we do different things on each
-    // button.
-    g_signal_connect(G_OBJECT(button), "button-press-event",
-                     G_CALLBACK(OnButtonPressed), this);
-    g_signal_connect(G_OBJECT(button), "button-release-event",
-                     G_CALLBACK(OnButtonReleased), this);
-    GTK_WIDGET_UNSET_FLAGS(button, GTK_CAN_FOCUS);
-  } else {
-    // TODO(erg): This button can also be a drop target.
-
-    // Connect to 'button-release-event' instead of 'clicked' because we need
-    // access to the modifier keys and we do different things on each
-    // button.
-    g_signal_connect(G_OBJECT(button), "button-press-event",
-                     G_CALLBACK(OnButtonPressed), this);
-    g_signal_connect(G_OBJECT(button), "button-release-event",
-                     G_CALLBACK(OnFolderButtonReleased), this);
-    GTK_WIDGET_UNSET_FLAGS(button, GTK_CAN_FOCUS);
-  }
+  ConnectFolderButtonEvents(button);
+  GTK_WIDGET_UNSET_FLAGS(button, GTK_CAN_FOCUS);
 
   return button;
 }
@@ -364,6 +343,16 @@ GtkToolItem* BookmarkBarGtk::CreateBookmarkToolItem(BookmarkNode* node) {
   return item;
 }
 
+void BookmarkBarGtk::ConnectFolderButtonEvents(GtkWidget* widget) {
+  // Connect to 'button-release-event' instead of 'clicked' because we need
+  // access to the modifier keys and we do different things on each
+  // button.
+  g_signal_connect(G_OBJECT(widget), "button-press-event",
+                   G_CALLBACK(OnButtonPressed), this);
+  g_signal_connect(G_OBJECT(widget), "button-release-event",
+                   G_CALLBACK(OnFolderButtonReleased), this);
+}
+
 std::string BookmarkBarGtk::BuildTooltip(BookmarkNode* node) {
   // TODO(erg): Actually build the tooltip. For now, we punt and just return
   // the URL.
@@ -371,7 +360,7 @@ std::string BookmarkBarGtk::BuildTooltip(BookmarkNode* node) {
 }
 
 BookmarkNode* BookmarkBarGtk::GetNodeForToolButton(GtkWidget* widget) {
-  // First check to see if |button| is the special cased.
+  // First check to see if |button| is special cased.
   if (widget == other_bookmarks_button_)
     return model_->other_node();
   else if (widget == bookmark_toolbar_.get())

chrome/browser/gtk/bookmark_bar_gtk.h
@@ -107,6 +107,8 @@ class BookmarkBarGtk : public BookmarkModelObserver {
   GtkWidget* CreateBookmarkButton(BookmarkNode* node);
   GtkToolItem* CreateBookmarkToolItem(BookmarkNode* node);
 
+  void ConnectFolderButtonEvents(GtkWidget* widget);
+
   std::string BuildTooltip(BookmarkNode* node);
 
   // Finds the BookmarkNode from the model associated with |button|.

commit 10fd52f4b14b6ec93f39821cea9ab9e23ac2397e

Fix regression introduced in r15560 where all bookmark buttons became folders.
http://crbug.com/11628

Review URL: http://codereview.chromium.org/113134

git-svn-id: http://src.chromium.org/svn/trunk/src@15642 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

chrome/browser/bookmarks/bookmark_menu_controller_gtk.cc
@@ -14,6 +14,8 @@
 #include "grit/theme_resources.h"
 #include "webkit/glue/window_open_disposition.h"
 
+const int kEmptyId = -1;
+
 BookmarkMenuController::BookmarkMenuController(Browser* browser,
                                                Profile* profile,
                                                PageNavigator* navigator,
@@ -52,8 +54,7 @@ void BookmarkMenuController::BookmarkNodeFavIconLoaded(BookmarkModel* model,
 }
 
 bool BookmarkMenuController::IsCommandEnabled(int id) const {
-  // -1 is reserved for empty.
-  return id != -1;
+  return id != kEmptyId;
 }
 
 void BookmarkMenuController::ExecuteCommand(int id) {
@@ -104,6 +105,6 @@ void BookmarkMenuController::BuildMenu(BookmarkNode* parent,
 
   if (parent->GetChildCount() == 0) {
     menu->AppendMenuItemWithLabel(
-        -1, l10n_util::GetStringUTF8(IDS_MENU_EMPTY_SUBMENU));
+        kEmptyId, l10n_util::GetStringUTF8(IDS_MENU_EMPTY_SUBMENU));
   }
 }

chrome/browser/gtk/bookmark_bar_gtk.cc
@@ -325,7 +325,19 @@ GtkWidget* BookmarkBarGtk::CreateBookmarkButton(
   g_signal_connect(G_OBJECT(button), "drag-end",
                    G_CALLBACK(&OnButtonDragEnd), this);
 
-  ConnectFolderButtonEvents(button);
+  if (node->is_url()) {
+    // Connect to 'button-release-event' instead of 'clicked' because we need
+    // access to the modifier keys and we do different things on each
+    // button.
+    g_signal_connect(G_OBJECT(button), "button-press-event",
+                     G_CALLBACK(OnButtonPressed), this);
+    g_signal_connect(G_OBJECT(button), "button-release-event",
+                     G_CALLBACK(OnButtonReleased), this);
+  } else {
+    // TODO(erg): This button can also be a drop target.
+    ConnectFolderButtonEvents(button);
+  }
+
   GTK_WIDGET_UNSET_FLAGS(button, GTK_CAN_FOCUS);
 
   return button;
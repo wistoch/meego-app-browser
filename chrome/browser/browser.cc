wistoch/meego-app-browser

commit 713bf7808c0e71c2b4ad8580989d24c2782dc0bf

Fixes to make a browser window come up with TOOLKIT_VIEWS.
TBR=sky
BUG=none
TEST=none

Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=17271
Review URL: http://codereview.chromium.org/116007

git-svn-id: http://src.chromium.org/svn/trunk/src@17285 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

chrome/browser/browser.cc
@@ -281,9 +281,11 @@ void Browser::CreateBrowserWindow() {
     window_->GetLocationBar()->ShowFirstRunBubble(show_OEM_bubble);
   }
 
+#if defined(OS_WIN)
   FindBar* find_bar = BrowserWindow::CreateFindBar(this);
   find_bar_controller_.reset(new FindBarController(find_bar));
   find_bar->SetFindBarController(find_bar_controller_.get());
+#endif
 }
 
 ///////////////////////////////////////////////////////////////////////////////

chrome/browser/views/frame/browser_view.cc
@@ -621,7 +621,11 @@ BrowserWindowTesting* BrowserView::GetBrowserWindowTesting() {
 }
 
 StatusBubble* BrowserView::GetStatusBubble() {
+#if !defined(TOOLKIT_VIEWS)
   return status_bubble_.get();
+#else
+  return NULL;
+#endif
 }
 
 void BrowserView::SelectedTabToolbarSizeChanged(bool is_animating) {
@@ -1333,6 +1337,7 @@ void BrowserView::Init() {
 }
 
 void BrowserView::InitSystemMenu() {
+#if defined(OS_WIN)
   system_menu_.reset(
       views::Menu::GetSystemMenu(frame_->GetWindow()->GetNativeWindow()));
   int insertion_index = std::max(0, system_menu_->ItemCount() - 1);
@@ -1352,6 +1357,7 @@ void BrowserView::InitSystemMenu() {
   } else {
     BuildMenuForTabStriplessWindow(system_menu_.get(), insertion_index);
   }
+#endif
 }
 
 int BrowserView::LayoutTabStrip() {

chrome/browser/views/infobars/infobars.cc
@@ -201,6 +201,7 @@ void InfoBar::AnimationEnded(const Animation* animation) {
 void InfoBar::InfoBarAdded() {
   // The container_ pointer must be set before adding to the view hierarchy.
   DCHECK(container_);
+#if defined(OS_WIN)
   // When we're added to a view hierarchy within a widget, we create an
   // external focus tracker to track what was focused in case we obtain
   // focus so that we can restore focus when we're removed.
@@ -210,6 +211,7 @@ void InfoBar::InfoBarAdded() {
         new views::ExternalFocusTracker(this,
             views::FocusManager::GetFocusManager(widget->GetNativeView())));
   }
+#endif
 }
 
 void InfoBar::InfoBarRemoved() {

chrome/browser/views/location_bar_view.cc
@@ -155,6 +155,7 @@ void LocationBarView::Init() {
   location_entry_.reset(new AutocompleteEditViewGtk(this, model_, profile_,
                                                     command_updater_,
                                                     popup_positioner_));
+  location_entry_->Init();
 #endif
   location_entry_view_ = new views::NativeViewHost;
   location_entry_view_->SetID(VIEW_ID_AUTOCOMPLETE);

chrome/browser/window_sizer.cc
@@ -111,6 +111,11 @@ void WindowSizer::GetBrowserWindowBounds(const std::wstring& app_name,
                                          Browser* browser,
                                          gfx::Rect* window_bounds,
                                          bool* maximized) {
+#if defined(TOOLKIT_VIEWS)
+  *window_bounds = gfx::Rect(20, 20, 500, 500);
+  *maximized = false;
+  return;  
+#endif
   const WindowSizer sizer(new DefaultStateProvider(app_name, browser),
                           CreateDefaultMonitorInfoProvider());
   sizer.DetermineWindowBounds(specified_bounds, window_bounds, maximized);

commit 0493676958506cae1ffeb7e7421ceb3c9beeefd3

Don't crash on find on OS X and Linux. Bug introduced by r17285.
BUG=none
TEST=Hit ctrl-f in Linux or OS X and check that chrome doesn't crash.

Review URL: http://codereview.chromium.org/115979

git-svn-id: http://src.chromium.org/svn/trunk/src@17298 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

chrome/browser/browser.cc
@@ -281,7 +281,7 @@ void Browser::CreateBrowserWindow() {
     window_->GetLocationBar()->ShowFirstRunBubble(show_OEM_bubble);
   }
 
-#if defined(OS_WIN)
+#if defined(OS_WIN) || !defined(TOOLKIT_VIEWS)
   FindBar* find_bar = BrowserWindow::CreateFindBar(this);
   find_bar_controller_.reset(new FindBarController(find_bar));
   find_bar->SetFindBarController(find_bar_controller_.get());
wistoch/meego-app-browser

commit 5e91ca06e09078ae746e819ca196d4c8f7a7e436

Use a saved copy of the time when pausing media elements.
Since our internal pause is asynchronous and we don't want to hang the render thread, it's possible to have our clock creep ahead.  This produces unexpected behaviour when pausing media elements, such as seeing currentTime attribute nudge a little ahead after calling pause().

This so happens to also address layout test flakiness.

BUG=13907
TEST=layout tests, especially video-pause-immediately.html -- a new layout test I wrote and am upstreaming

Review URL: http://codereview.chromium.org/334002


git-svn-id: http://src.chromium.org/svn/trunk/src@29851 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

webkit/glue/webmediaplayer_impl.cc
@@ -260,6 +260,7 @@ void WebMediaPlayerImpl::pause() {
 
   paused_ = true;
   pipeline_->SetPlaybackRate(0.0f);
+  paused_time_ = pipeline_->GetCurrentTime();
 }
 
 bool WebMediaPlayerImpl::supportsFullscreen() const {
@@ -398,6 +399,9 @@ float WebMediaPlayerImpl::duration() const {
 float WebMediaPlayerImpl::currentTime() const {
   DCHECK(MessageLoop::current() == main_loop_);
 
+  if (paused_) {
+    return static_cast<float>(paused_time_.InSecondsF());
+  }
   return static_cast<float>(pipeline_->GetCurrentTime().InSecondsF());
 }
 

webkit/glue/webmediaplayer_impl.h
@@ -271,8 +271,15 @@ class WebMediaPlayerImpl : public WebKit::WebMediaPlayer,
   // single "playback rate" over worrying about paused/stopped etc...  It forces
   // all clients to manage the pause+playback rate externally, but is that
   // really a bad thing?
+  //
+  // TODO(scherkus): since SetPlaybackRate(0) is asynchronous and we don't want
+  // to hang the render thread during pause(), we record the time at the same
+  // time we pause and then return that value in currentTime().  Otherwise our
+  // clock can creep forward a little bit while the asynchronous
+  // SetPlaybackRate(0) is being executed.
   bool paused_;
   float playback_rate_;
+  base::TimeDelta paused_time_;
 
   WebKit::WebMediaPlayerClient* client_;
 

commit 0a7dfa6914bb817f5be535545a336ab3c108571c

Update the paused time when seeking while paused.
Fixes regression caused by r29851.

BUG=13907
TEST=video-seeking.html should start passing reliably

Review URL: http://codereview.chromium.org/332017


git-svn-id: http://src.chromium.org/svn/trunk/src@29983 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

webkit/glue/webmediaplayer_impl.cc
@@ -295,8 +295,17 @@ void WebMediaPlayerImpl::seek(float seconds) {
 
   // Try to preserve as much accuracy as possible.
   float microseconds = seconds * base::Time::kMicrosecondsPerSecond;
+  base::TimeDelta seek_time =
+      base::TimeDelta::FromMicroseconds(static_cast<int64>(microseconds));
+
+  // Update our paused time.
+  if (paused_) {
+    paused_time_ = seek_time;
+  }
+
+  // Kick off the asynchronous seek!
   pipeline_->Seek(
-      base::TimeDelta::FromMicroseconds(static_cast<int64>(microseconds)),
+      seek_time,
       NewCallback(proxy_.get(),
                   &WebMediaPlayerImpl::Proxy::PipelineSeekCallback));
 }
@@ -558,6 +567,11 @@ void WebMediaPlayerImpl::OnPipelineInitialize() {
 void WebMediaPlayerImpl::OnPipelineSeek() {
   DCHECK(MessageLoop::current() == main_loop_);
   if (pipeline_->GetError() == media::PIPELINE_OK) {
+    // Update our paused time.
+    if (paused_) {
+      paused_time_ = pipeline_->GetCurrentTime();
+    }
+
     SetReadyState(WebKit::WebMediaPlayer::HaveEnoughData);
     GetClient()->timeChanged();
   }
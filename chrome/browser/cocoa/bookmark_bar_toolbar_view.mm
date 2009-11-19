wistoch/meego-app-browser

commit 50a07c8bfb329571d64d92e8060010cc80b8b3c6

Mac: animate NTP detached bookmark bar <-> normal bookmark bar.
Also mostly fix the placement of the "For quick access, place your bookmarks here [...]" message (when there are no bookmarks). It's not quite right in the new animation (detached <-> normal).

BUG=25600, 27692, 27693
TEST=Go to NTP and press Cmd-Shift-B a few times while watching closely. The detached bar should "morph" smoothly to the normal bar and vice versa. Switch themes and repeat; please test at least the default theme and the Brushed theme.

Review URL: http://codereview.chromium.org/400020

git-svn-id: http://src.chromium.org/svn/trunk/src@32425 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

chrome/browser/cocoa/bookmark_bar_toolbar_view.mm
@@ -23,11 +23,13 @@ const CGFloat kBorderRadius = 3.0;
 @implementation BookmarkBarToolbarView
 
 - (BOOL)isOpaque {
-  return [controller_ isShownAsDetachedBar];
+  return [controller_ isInState:bookmarks::kDetachedState];
 }
 
 - (void)drawRect:(NSRect)rect {
-  if ([controller_ isShownAsDetachedBar]) {
+  if ([controller_ isInState:bookmarks::kDetachedState] ||
+      [controller_ isAnimatingToState:bookmarks::kDetachedState] ||
+      [controller_ isAnimatingFromState:bookmarks::kDetachedState]) {
     [self drawRectAsBubble:rect];
   } else {
     NSPoint phase = [self gtm_themePatternPhase];
@@ -37,16 +39,20 @@ const CGFloat kBorderRadius = 3.0;
 }
 
 - (void)drawRectAsBubble:(NSRect)rect {
+  // The state of our morph; 1 is total bubble, 0 is the regular bar. We use it
+  // to morph the bubble to a regular bar (shape and colour).
+  CGFloat morph = [controller_ detachedMorphProgress];
+
   NSRect bounds = [self bounds];
 
   ThemeProvider* themeProvider = [controller_ themeProvider];
   if (!themeProvider)
     return;
 
-  NSGraphicsContext* theContext = [NSGraphicsContext currentContext];
-  [theContext saveGraphicsState];
+  NSGraphicsContext* context = [NSGraphicsContext currentContext];
+  [context saveGraphicsState];
 
-  // Draw the background
+  // Draw the background.
   {
     // CanvasPaint draws to the NSGraphicsContext during its destructor, so
     // explicitly scope this.
@@ -69,16 +75,19 @@ const CGFloat kBorderRadius = 3.0;
 
   // Draw our bookmark bar border on top of the background.
   NSRect frameRect =
-      NSMakeRect(bookmarks::kNTPBookmarkBarPadding,
-                 bookmarks::kNTPBookmarkBarPadding,
-                 NSWidth(bounds) - 2 * bookmarks::kNTPBookmarkBarPadding,
-                 NSHeight(bounds) - 2 * bookmarks::kNTPBookmarkBarPadding);
-  // Now draw a bezier path with rounded rectangles around the area
-  frameRect = NSInsetRect(frameRect, 0.5, 0.5);
+      NSMakeRect(
+          morph * bookmarks::kNTPBookmarkBarPadding,
+          morph * bookmarks::kNTPBookmarkBarPadding,
+          NSWidth(bounds) - 2 * morph * bookmarks::kNTPBookmarkBarPadding,
+          NSHeight(bounds) - 2 * morph * bookmarks::kNTPBookmarkBarPadding);
+  // Now draw a bezier path with rounded rectangles around the area.
+  frameRect = NSInsetRect(frameRect, morph * 0.5, morph * 0.5);
   NSBezierPath* border =
       [NSBezierPath bezierPathWithRoundedRect:frameRect
-                                      xRadius:kBorderRadius
-                                      yRadius:kBorderRadius];
+                                      xRadius:(morph * kBorderRadius)
+                                      yRadius:(morph * kBorderRadius)];
+
+  // Draw the rounded rectangle.
   NSColor* toolbarColor =
       [[self gtm_theme] backgroundColorForStyle:GTMThemeStyleToolBar
                                           state:GTMThemeStateActiveWindow];
@@ -89,16 +98,43 @@ const CGFloat kBorderRadius = 3.0;
     toolbarColor = nil;
   if (!toolbarColor)
     toolbarColor = [NSColor colorWithCalibratedWhite:0.9 alpha:1.0];
-  [toolbarColor set];
+  [[toolbarColor colorWithAlphaComponent:morph] set];  // Set with opacity.
   [border fill];
 
+  // Fade in/out the background.
+  [context saveGraphicsState];
+  [border setClip];
+  CGContextRef cgContext = (CGContextRef)[context graphicsPort];
+  CGContextBeginTransparencyLayer(cgContext, NULL);
+  CGContextSetAlpha(cgContext, 1 - morph);
+  [context setPatternPhase:[self gtm_themePatternPhase]];
+  [self drawBackground];
+  CGContextEndTransparencyLayer(cgContext);
+  [context restoreGraphicsState];
+
+  // Draw the border of the rounded rectangle.
   NSColor* borderColor =
       [[self gtm_theme] strokeColorForStyle:GTMThemeStyleToolBarButton
                                       state:GTMThemeStateActiveWindow];
-  [borderColor set];
+  [[borderColor colorWithAlphaComponent:morph] set];  // Set with opacity.
   [border stroke];
 
-  [theContext restoreGraphicsState];
+  // Fade in/out the divider.
+  // TODO(viettrungluu): It's not obvious that this divider lines up exactly
+  // with |BackgroundGradientView|'s (in fact, it probably doesn't).
+  [[[self strokeColor] colorWithAlphaComponent:(1 - morph)] set];
+  NSBezierPath* divider = [NSBezierPath bezierPath];
+  NSPoint dividerStart =
+      NSMakePoint(morph * bookmarks::kNTPBookmarkBarPadding + morph * 0.5,
+                  morph * bookmarks::kNTPBookmarkBarPadding + morph * 0.5);
+  CGFloat dividerWidth =
+      NSWidth(bounds) - 2 * morph * bookmarks::kNTPBookmarkBarPadding - 2 * 0.5;
+  [divider moveToPoint:dividerStart];
+  [divider relativeLineToPoint:NSMakePoint(dividerWidth, 0)];
+  [divider stroke];
+
+  // Restore the graphics context.
+  [context restoreGraphicsState];
 }
 
 @end  // @implementation BookmarkBarToolbarView

chrome/browser/cocoa/animatable_view.h
@@ -30,7 +30,8 @@
 }
 
 // Properties for bindings.
-@property(assign) id delegate;
+@property(assign, nonatomic) id delegate;
+@property(assign, nonatomic) id<ViewResizer> resizeDelegate;
 
 // Gets the current height of the view.  If an animation is currently running,
 // this will give the current height at the time of the call, not the target
@@ -49,9 +50,8 @@
 // (mid-animation) height.
 - (void)stopAnimation;
 
-// Sets the delegate that gets notified when this view needs to chanage its
-// height.
-- (void)setResizeDelegate:(id<ViewResizer>)resizeDelegate;
+// Gets the progress of any current animation.
+- (NSAnimationProgress)currentAnimationProgress;
 
 @end
 

chrome/browser/cocoa/animatable_view.mm
@@ -49,6 +49,7 @@
 
 @implementation AnimatableView
 @synthesize delegate = delegate_;
+@synthesize resizeDelegate = resizeDelegate_;
 
 - (void)dealloc {
   // Stop the animation if it is running, since it holds a pointer to this view.
@@ -84,8 +85,8 @@
   [currentAnimation_ stopAnimation];
 }
 
-- (void)setResizeDelegate:(id<ViewResizer>)resizeDelegate {
-  resizeDelegate_ = resizeDelegate;
+- (NSAnimationProgress)currentAnimationProgress {
+  return [currentAnimation_ currentProgress];
 }
 
 - (void)animationDidStop:(NSAnimation*)animation {

chrome/browser/cocoa/bookmark_bar_controller.h
@@ -11,6 +11,7 @@
 #include "base/scoped_nsobject.h"
 #include "base/scoped_ptr.h"
 #include "chrome/browser/cocoa/bookmark_bar_bridge.h"
+#import "chrome/browser/cocoa/bookmark_bar_state.h"
 #import "chrome/browser/cocoa/bookmark_bar_toolbar_view.h"
 #include "chrome/browser/cocoa/tab_strip_model_observer_bridge.h"
 #include "webkit/glue/window_open_disposition.h"
@@ -38,24 +39,15 @@ const CGFloat kBookmarkVerticalPadding = 2.0;
 const CGFloat kBookmarkHorizontalPadding = 1.0;
 
 const CGFloat kNoBookmarksHorizontalOffset = 5.0;
-const CGFloat kNoBookmarksVerticalOffset = 22.0;
-const CGFloat kNoBookmarksNTPVerticalOffset = 28.0;
-
-// States for the bookmark bar.
-enum VisualState {
-  kInvalidState  = 0,
-  kHiddenState   = 1,
-  kShowingState  = 2,
-  kDetachedState = 3,
-};
+const CGFloat kNoBookmarksVerticalOffset = 6.0;
 
 }  // namespace bookmarks
 
 // The interface for the bookmark bar controller's delegate. Currently, the
 // delegate is the BWC and is responsible for ensuring that the toolbar is
 // displayed correctly (as specified by |-getDesiredToolbarHeightCompression|
-// and |-shouldToolbarShowDivider|) at the beginning and at the end of an
-// animation (or after a state change).
+// and |-toolbarDividerOpacity|) at the beginning and at the end of an animation
+// (or after a state change).
 @protocol BookmarkBarControllerDelegate
 
 // Sent when the state has changed (after any animation), but before the final
@@ -74,7 +66,7 @@ willAnimateFromState:(bookmarks::VisualState)oldState
 // A controller for the bookmark bar in the browser window. Handles showing
 // and hiding based on the preference in the given profile.
 @interface BookmarkBarController :
-    NSViewController<BookmarkBarToolbarViewController> {
+    NSViewController<BookmarkBarState, BookmarkBarToolbarViewController> {
  @private
   // The visual state of the bookmark bar. If an animation is running, this is
   // set to the "destination" and |lastVisualState_| is set to the "original"
@@ -160,30 +152,20 @@ willAnimateFromState:(bookmarks::VisualState)oldState
 // if needed.  For fullscreen mode.
 - (void)setBookmarkBarEnabled:(BOOL)enabled;
 
-// Returns YES if the bookmarks bar is currently visible (as a normal toolbar or
-// as a detached bar on the NTP), NO otherwise.
-- (BOOL)isVisible;
-
-// Returns YES if an animation is currently running, NO otherwise.
-- (BOOL)isAnimationRunning;
-
-// Returns YES if the bookmarks bar is (to be) shown as part of the normal
-// toolbar, NO otherwise. This is exclusive of |-isShownAsDetachedBar|.
-- (BOOL)isShownAsToolbar;
-
-// Returns YES if the bookmarks bar is (to be) shown as a detached bar, NO
-// otherwise; required for the |BookmarkBarToolbarViewController| protocol. This
-// is exclusive of |-isShownAsToolbar|.
-- (BOOL)isShownAsDetachedBar;
-
 // Returns the amount by which the toolbar above should be compressed.
 - (CGFloat)getDesiredToolbarHeightCompression;
 
-// Returns whether or not the toolbar above should show the divider.
-- (BOOL)shouldToolbarShowDivider;
+// Gets the appropriate opacity for the toolbar's divider; 0 means that it
+// shouldn't be shown.
+- (CGFloat)toolbarDividerOpacity;
 
 // Returns true if at least one bookmark was added.
 - (BOOL)addURLs:(NSArray*)urls withTitles:(NSArray*)titles at:(NSPoint)point;
+
+// Updates the sizes and positions of the subviews.
+// TODO(viettrungluu): I'm not convinced this should be public, but I currently
+// need it for animations. Try not to propagate its use.
+- (void)layoutSubviews;
 
 // Complete a drag of a bookmark button to this location on the main bar.
 // TODO(jrg): submenu DnD.

commit 03d958088a068721a0cc4853675ad9fe6e41e715

Mac: Fix regression in NTP detached bookmark bar colours.
The regression was caused by r32425.

BUG=28203
TEST=Look at NTP detached bookmark bar: it should be medium-ish grey, not black (or is that dark grey)?

Review URL: http://codereview.chromium.org/406009

git-svn-id: http://src.chromium.org/svn/trunk/src@32461 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

chrome/browser/cocoa/bookmark_bar_toolbar_view.mm
@@ -98,7 +98,8 @@ const CGFloat kBorderRadius = 3.0;
     toolbarColor = nil;
   if (!toolbarColor)
     toolbarColor = [NSColor colorWithCalibratedWhite:0.9 alpha:1.0];
-  [[toolbarColor colorWithAlphaComponent:morph] set];  // Set with opacity.
+  CGFloat alpha = morph * [toolbarColor alphaComponent];
+  [[toolbarColor colorWithAlphaComponent:alpha] set];  // Set with opacity.
   [border fill];
 
   // Fade in/out the background.
@@ -116,13 +117,16 @@ const CGFloat kBorderRadius = 3.0;
   NSColor* borderColor =
       [[self gtm_theme] strokeColorForStyle:GTMThemeStyleToolBarButton
                                       state:GTMThemeStateActiveWindow];
-  [[borderColor colorWithAlphaComponent:morph] set];  // Set with opacity.
+  alpha = morph * [borderColor alphaComponent];
+  [[borderColor colorWithAlphaComponent:alpha] set];  // Set with opacity.
   [border stroke];
 
   // Fade in/out the divider.
   // TODO(viettrungluu): It's not obvious that this divider lines up exactly
   // with |BackgroundGradientView|'s (in fact, it probably doesn't).
-  [[[self strokeColor] colorWithAlphaComponent:(1 - morph)] set];
+  NSColor* strokeColor = [self strokeColor];
+  alpha = (1 - morph) * [strokeColor alphaComponent];
+  [[strokeColor colorWithAlphaComponent:alpha] set];
   NSBezierPath* divider = [NSBezierPath bezierPath];
   NSPoint dividerStart =
       NSMakePoint(morph * bookmarks::kNTPBookmarkBarPadding + morph * 0.5,
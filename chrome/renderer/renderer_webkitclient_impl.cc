wistoch/meego-app-browser

commit ebfecac3cdc73483a0053e0335c3731c8a740fba

Modal loops of joy.
We need to pump messages in the renderer and in any related plugin processes
when sending sync IPCs to get cookies and set localstorage items because these
IPCs can block on a user prompt.

This code moves complexity into RenderThread::Send().  That function is now
responsible for calling WebView::{willEnter,didExit}ModalLoop() and for
broadcasting the messages to signal/reset modal dialog events in the related
plugin processes.

This change also adds code to optionally suspend the shared webkit timer.  This
was needed to fix bug 36063.  It also helps make spinning a nested loop to wait
for document.cookie results a bit less risky since we'll be re-entering WebKit
in fewer cases.

R=jam
BUG=34917,36063
TEST=none

Review URL: http://codereview.chromium.org/607011

git-svn-id: http://src.chromium.org/svn/trunk/src@39327 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

chrome/renderer/renderer_webkitclient_impl.cc
@@ -116,13 +116,7 @@ void RendererWebKitClientImpl::setCookies(const WebURL& url,
                                           const WebString& value) {
   // TODO(darin): Modify WebKit to pass the WebFrame.  This code may be reached
   // when there is no active script context.
-  int routing_id = MSG_ROUTING_CONTROL;
-  if (v8::Context::InContext()) {
-    RenderView* view =
-        RenderView::FromWebView(WebFrame::frameForCurrentContext()->view());
-    DCHECK(view);
-    routing_id = view->routing_id();
-  }
+  int32 routing_id = RenderThread::RoutingIDForCurrentContext();
 
   std::string value_utf8;
   UTF16ToUTF8(value.data(), value.length(), &value_utf8);
@@ -133,9 +127,15 @@ void RendererWebKitClientImpl::setCookies(const WebURL& url,
 
 WebString RendererWebKitClientImpl::cookies(
     const WebURL& url, const WebURL& first_party_for_cookies) {
+  // TODO(darin): Modify WebKit to pass the WebFrame.  This code may be reached
+  // when there is no active script context.
+  int32 routing_id = RenderThread::RoutingIDForCurrentContext();
+
   std::string value_utf8;
-  RenderThread::current()->Send(
-      new ViewHostMsg_GetCookies(url, first_party_for_cookies, &value_utf8));
+  RenderThread::current()->SendAndRunNestedMessageLoop(
+      new ViewHostMsg_GetCookies(routing_id, url, first_party_for_cookies,
+                                 &value_utf8));
+
   return WebString::fromUTF8(value_utf8);
 }
 
@@ -143,9 +143,15 @@ bool RendererWebKitClientImpl::rawCookies(
     const WebURL& url,
     const WebURL& first_party_for_cookies,
     WebVector<WebKit::WebCookie>* raw_cookies) {
+  // TODO(darin): Modify WebKit to pass the WebFrame.  This code may be reached
+  // when there is no active script context.
+  int32 routing_id = RenderThread::RoutingIDForCurrentContext();
+
   std::vector<webkit_glue::WebCookie> cookies;
-  RenderThread::current()->Send(
-      new ViewHostMsg_GetRawCookies(url, first_party_for_cookies, &cookies));
+  RenderThread::current()->SendAndRunNestedMessageLoop(
+      new ViewHostMsg_GetRawCookies(routing_id, url, first_party_for_cookies,
+                                    &cookies));
+
 
   WebVector<WebKit::WebCookie> result(cookies.size());
   int i = 0;

chrome/common/child_thread.cc
@@ -95,6 +95,12 @@ void ChildThread::RemoveRoute(int32 routing_id) {
   router_.RemoveRoute(routing_id);
 }
 
+IPC::Channel::Listener* ChildThread::ResolveRoute(int32 routing_id) {
+  DCHECK(MessageLoop::current() == message_loop());
+
+  return router_.ResolveRoute(routing_id);
+}
+
 webkit_glue::ResourceLoaderBridge* ChildThread::CreateBridge(
     const webkit_glue::ResourceLoaderBridge::RequestInfo& request_info,
     int host_renderer_id,

chrome/common/child_thread.h
@@ -32,6 +32,8 @@ class ChildThread : public IPC::Channel::Listener,
   void AddRoute(int32 routing_id, IPC::Channel::Listener* listener);
   void RemoveRoute(int32 routing_id);
 
+  IPC::Channel::Listener* ResolveRoute(int32 routing_id);
+
   // Creates a ResourceLoaderBridge.
   // Tests can override this method if they want a custom loading behavior.
   virtual webkit_glue::ResourceLoaderBridge* CreateBridge(

chrome/common/message_router.cc
@@ -33,10 +33,14 @@ void MessageRouter::OnMessageReceived(const IPC::Message& msg) {
 }
 
 bool MessageRouter::RouteMessage(const IPC::Message& msg) {
-  IPC::Channel::Listener* listener = routes_.Lookup(msg.routing_id());
+  IPC::Channel::Listener* listener = ResolveRoute(msg.routing_id());
   if (!listener)
     return false;
 
   listener->OnMessageReceived(msg);
   return true;
+}
+
+IPC::Channel::Listener* MessageRouter::ResolveRoute(int32 routing_id) {
+  return routes_.Lookup(routing_id);
 }

chrome/common/message_router.h
@@ -50,6 +50,8 @@ class MessageRouter : public IPC::Channel::Listener,
   void AddRoute(int32 routing_id, IPC::Channel::Listener* listener);
   void RemoveRoute(int32 routing_id);
 
+  IPC::Channel::Listener* ResolveRoute(int32 routing_id);
+
  private:
   // A list of all listeners with assigned routing IDs.
   IDMap<IPC::Channel::Listener> routes_;

chrome/common/render_messages_internal.h
@@ -1112,18 +1112,20 @@ IPC_BEGIN_MESSAGES(ViewHost)
                       GURL /* first_party_for_cookies */,
                       std::string /* cookie */)
 
-  // Used to get cookies for the given URL
-  IPC_SYNC_MESSAGE_CONTROL2_1(ViewHostMsg_GetCookies,
-                              GURL /* url */,
-                              GURL /* first_party_for_cookies */,
-                              std::string /* cookies */)
+  // Used to get cookies for the given URL.  This may be blocked by a user
+  // prompt to validate a previous SetCookie message.
+  IPC_SYNC_MESSAGE_ROUTED2_1(ViewHostMsg_GetCookies,
+                             GURL /* url */,
+                             GURL /* first_party_for_cookies */,
+                             std::string /* cookies */)
 
-  // Used to get raw cookie information for the given URL
-  IPC_SYNC_MESSAGE_CONTROL2_1(ViewHostMsg_GetRawCookies,
-                              GURL /* url */,
-                              GURL /* first_party_for_cookies */,
-                              std::vector<webkit_glue::WebCookie>
-                                  /* raw_cookies */)
+  // Used to get raw cookie information for the given URL.  This may be blocked
+  // by a user prompt to validate a previous SetCookie message.
+  IPC_SYNC_MESSAGE_ROUTED2_1(ViewHostMsg_GetRawCookies,
+                             GURL /* url */,
+                             GURL /* first_party_for_cookies */,
+                             std::vector<webkit_glue::WebCookie>
+                                 /* raw_cookies */)
 
   // Used to delete cookie for the given URL and name
   IPC_SYNC_MESSAGE_CONTROL2_0(ViewHostMsg_DeleteCookie,
@@ -1458,10 +1460,10 @@ IPC_BEGIN_MESSAGES(ViewHost)
   // by javascript. This step is about showing UI to the user to select the
   // final print settings. The output parameter is the same as
   // ViewMsg_PrintPages which is executed implicitly.
-  IPC_SYNC_MESSAGE_CONTROL1_1(ViewHostMsg_ScriptedPrint,
-                              ViewHostMsg_ScriptedPrint_Params,
-                              ViewMsg_PrintPages_Params /* settings choosen by
-                                                          the user*/)
+  IPC_SYNC_MESSAGE_ROUTED1_1(ViewHostMsg_ScriptedPrint,
+                             ViewHostMsg_ScriptedPrint_Params,
+                             ViewMsg_PrintPages_Params
+                                 /* settings choosen by the user*/)
 #endif  // defined(OS_WIN) || defined(OS_MACOSX)
 
   // WebKit and JavaScript error messages to log to the console
@@ -1981,13 +1983,13 @@ IPC_BEGIN_MESSAGES(ViewHost)
                               NullableString16 /* value */)
 
   // Set a value that's associated with a key in a storage area.
-  IPC_SYNC_MESSAGE_CONTROL4_2(ViewHostMsg_DOMStorageSetItem,
-                              int64 /* storage_area_id */,
-                              string16 /* key */,
-                              string16 /* value */,
-                              GURL /* url */,
-                              WebKit::WebStorageArea::Result /* result */,
-                              NullableString16 /* old_value */)
+  IPC_SYNC_MESSAGE_ROUTED4_2(ViewHostMsg_DOMStorageSetItem,
+                             int64 /* storage_area_id */,
+                             string16 /* key */,
+                             string16 /* value */,
+                             GURL /* url */,
+                             WebKit::WebStorageArea::Result /* result */,
+                             NullableString16 /* old_value */)
 
   // Remove the value associated with a key in a storage area.
   IPC_SYNC_MESSAGE_CONTROL3_1(ViewHostMsg_DOMStorageRemoveItem,

chrome/common/resource_dispatcher.cc
@@ -223,8 +223,9 @@ void IPCResourceLoaderBridge::SyncLoad(SyncLoadResponse* response) {
   request_id_ = MakeRequestID();
 
   SyncLoadResult result;
-  IPC::Message* msg = new ViewHostMsg_SyncLoad(routing_id_, request_id_,
-                                               request_, &result);
+  IPC::SyncMessage* msg = new ViewHostMsg_SyncLoad(routing_id_, request_id_,
+                                                   request_, &result);
+  msg->EnableMessagePumping();
   if (!dispatcher_->message_sender()->Send(msg)) {
     response->status.set_status(URLRequestStatus::FAILED);
     return;

chrome/renderer/print_web_view_helper_mac.mm
@@ -102,8 +102,10 @@ void PrintWebViewHelper::Print(WebFrame* frame, bool script_initiated) {
       params.has_selection = frame->hasSelection();
       params.expected_pages_count = expected_pages_count;
 
-      msg = new ViewHostMsg_ScriptedPrint(params, &print_settings);
-      if (render_view_->SendAndRunNestedMessageLoop(msg)) {
+      msg = new ViewHostMsg_ScriptedPrint(routing_id(), params,
+                                          &print_settings);
+      msg->EnableMessagePumping();
+      if (Send(msg)) {
         msg = NULL;
 
         // If the settings are invalid, early quit.

commit 036e88ae3451d2ebf323d9ca1e720346a7ee11e3

Suppress calls to willEnter/didExitModalLoop to avoid deferring resource loads while querying cookies.
This is another attempt to isolate the Intl2 performance regression caused by
r39327.

TBR=jam
BUG=36310
TEST=none

Review URL: http://codereview.chromium.org/654002

git-svn-id: http://src.chromium.org/svn/trunk/src@39706 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

chrome/renderer/renderer_webkitclient_impl.cc
@@ -131,6 +131,12 @@ WebString RendererWebKitClientImpl::cookies(
   // when there is no active script context.
   int32 routing_id = RenderThread::RoutingIDForCurrentContext();
 
+  // TODO(darin): This means that network load completions may run within the
+  // context of a document.cookie call.  This violates the "run to completion"
+  // gaurantee of JS.  This is just here as a temporary measure to isolate a
+  // performance regression.
+  RenderThread::current()->DoNotNotifyWebKitOfModalLoop();
+
   std::string value_utf8;
   RenderThread::current()->SendAndRunNestedMessageLoop(
       new ViewHostMsg_GetCookies(routing_id, url, first_party_for_cookies,
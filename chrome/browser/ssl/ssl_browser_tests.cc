wistoch/meego-app-browser

commit e7e937cbf9fa2985749caf3efa2691270a2d6c9e

[Mac] Honor modifier keys in back/forward menu.
Add a method ActivatedAtWithDisposition to BackForwardMenuModel that navigates to a history item with a certain window disposition (current tab, new background/foreground tab).

Change BackForwardMenuController to call ActivatedAtWithDisposition with the disposition created from the current NSEvent.

Also, factor out some common code in Browser::GoBack and GoForward.

BUG=37984
TEST=Navigate to a history item in the back/forward menu while holding down the command key. The item should open in a new tab. Likewise, holding down cmd-shift should open it in a new foreground tab.

Review URL: http://codereview.chromium.org/910001

git-svn-id: http://src.chromium.org/svn/trunk/src@41875 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

chrome/browser/browser.cc
@@ -788,6 +788,19 @@ bool Browser::CanRestoreTab() {
   return service && !service->entries().empty();
 }
 
+bool Browser::NavigateToIndexWithDisposition(int index,
+                                             WindowOpenDisposition disp) {
+  NavigationController& controller =
+      GetOrCloneNavigationControllerForDisposition(disp);
+
+  if (index >= 0 && index < controller.entry_count()) {
+    controller.GoToIndex(index);
+    return true;
+  } else {
+    return false;
+  }
+}
+
 void Browser::ShowSingletonTab(const GURL& url) {
   // See if we already have a tab with the given URL and select it if so.
   for (int i = 0; i < tabstrip_model_.count(); i++) {
@@ -842,49 +855,43 @@ void Browser::UpdateCommandsForFullscreenMode(bool is_fullscreen) {
 ///////////////////////////////////////////////////////////////////////////////
 // Browser, Assorted browser commands:
 
+NavigationController& Browser::GetOrCloneNavigationControllerForDisposition(
+       WindowOpenDisposition disposition) {
+  TabContents* current_tab = GetSelectedTabContents();
+  if (disposition == NEW_FOREGROUND_TAB ||
+      disposition == NEW_BACKGROUND_TAB) {
+    TabContents* cloned = current_tab->Clone();
+    tabstrip_model_.AddTabContents(cloned, -1, false,
+                                   PageTransition::LINK,
+                                   disposition == NEW_FOREGROUND_TAB);
+    return cloned->controller();
+  } else {
+    // Default disposition is CURRENT_TAB.
+    return current_tab->controller();
+  }
+}
+
 void Browser::GoBack(WindowOpenDisposition disposition) {
   UserMetrics::RecordAction("Back", profile_);
 
   TabContents* current_tab = GetSelectedTabContents();
   if (current_tab->controller().CanGoBack()) {
-    NavigationController* controller = NULL;
-    if (disposition == NEW_FOREGROUND_TAB ||
-        disposition == NEW_BACKGROUND_TAB) {
-      TabContents* cloned = GetSelectedTabContents()->Clone();
-      tabstrip_model_.AddTabContents(cloned, -1, false,
-                                     PageTransition::LINK,
-                                     disposition == NEW_FOREGROUND_TAB);
-      if (current_tab->interstitial_page()) {
-        // The interstitial won't be copied to the new tab, so we don't need to
-        // go back.
-        return;
-      }
-      controller = &cloned->controller();
-    } else {
-      // Default disposition is CURRENT_TAB.
-      controller = &current_tab->controller();
+    NavigationController& controller =
+        GetOrCloneNavigationControllerForDisposition(disposition);
+    // The interstitial won't be copied to the new tab, so we don't need to
+    // go back.
+    if (!current_tab->interstitial_page()) {
+      controller.GoBack();
     }
-    controller->GoBack();
   }
 }
 
-void Browser::GoForward(WindowOpenDisposition disp) {
-  // TODO(brettw) this is mostly duplicated from GoBack, these should have a
-  // common backend or something.
+void Browser::GoForward(WindowOpenDisposition disposition) {
   UserMetrics::RecordAction("Forward", profile_);
   if (GetSelectedTabContents()->controller().CanGoForward()) {
-    NavigationController* controller = 0;
-    if (disp == NEW_FOREGROUND_TAB || disp == NEW_BACKGROUND_TAB) {
-      TabContents* cloned = GetSelectedTabContents()->Clone();
-      tabstrip_model_.AddTabContents(cloned, -1, false,
-                                     PageTransition::LINK,
-                                     disp == NEW_FOREGROUND_TAB);
-      controller = &cloned->controller();
-    } else {
-      // Default disposition is CURRENT_TAB.
-      controller = &GetSelectedTabContents()->controller();
-    }
-    controller->GoForward();
+    NavigationController& controller =
+        GetOrCloneNavigationControllerForDisposition(disposition);
+    controller.GoForward();
   }
 }
 

chrome/browser/browser.h
@@ -349,6 +349,10 @@ class Browser : public TabStripModelDelegate,
   // Returns true if a tab can be restored.
   virtual bool CanRestoreTab();
 
+  // Navigate to an index in the tab history, opening a new tab depending on the
+  // disposition.
+  bool NavigateToIndexWithDisposition(int index, WindowOpenDisposition disp);
+
   // Show a given a URL. If a tab with the same URL (ignoring the ref) is
   // already visible in this browser, it becomes selected. Otherwise a new tab
   // is created.
@@ -796,6 +800,11 @@ class Browser : public TabStripModelDelegate,
 
   // Shared code between Reload() and ReloadAll().
   void ReloadInternal(bool ignore_cache);
+
+  // Depending on the disposition, return the |NavigationController| for the
+  // current tab or clone the current tab and return its |NavigationController|.
+  NavigationController& GetOrCloneNavigationControllerForDisposition(
+       WindowOpenDisposition disp);
 
   // Data members /////////////////////////////////////////////////////////////
 

chrome/browser/back_forward_menu_model.cc
@@ -127,14 +127,20 @@ void BackForwardMenuModel::HighlightChangedTo(int index) {
 }
 
 void BackForwardMenuModel::ActivatedAt(int index) {
-  NavigationController& controller = GetTabContents()->controller();
+  ActivatedAtWithDisposition(index, CURRENT_TAB);
+}
+
+void BackForwardMenuModel::ActivatedAtWithDisposition(
+      int index,
+      WindowOpenDisposition disposition) {
+  Profile* profile = browser_->profile();
 
   DCHECK(!IsSeparator(index));
 
   // Execute the command for the last item: "Show Full History".
   if (index == GetItemCount() - 1) {
     UserMetrics::RecordComputedAction(BuildActionName("ShowFullHistory", -1),
-                                      controller.profile());
+                                      profile);
     browser_->ShowSingletonTab(GURL(chrome::kChromeUIHistoryURL));
     return;
   }
@@ -142,18 +148,18 @@ void BackForwardMenuModel::ActivatedAt(int index) {
   // Log whether it was a history or chapter click.
   if (index < GetHistoryItemCount()) {
     UserMetrics::RecordComputedAction(
-        BuildActionName("HistoryClick", index), controller.profile());
+        BuildActionName("HistoryClick", index), profile);
   } else {
     UserMetrics::RecordComputedAction(
         BuildActionName("ChapterClick", index - GetHistoryItemCount() - 1),
-        controller.profile());
+        profile);
   }
 
   int controller_index = MenuIndexToNavEntryIndex(index);
-  if (controller_index >= 0 && controller_index < controller.entry_count())
-    controller.GoToIndex(controller_index);
-  else
+  if (!browser_->NavigateToIndexWithDisposition(controller_index,
+                                                disposition)) {
     NOTREACHED();
+  }
 }
 
 void BackForwardMenuModel::MenuWillShow() {

chrome/browser/back_forward_menu_model.h
@@ -11,6 +11,7 @@
 #include "base/basictypes.h"
 #include "base/string16.h"
 #include "testing/gtest/include/gtest/gtest_prod.h"  // For FRIEND_TEST
+#include "webkit/glue/window_open_disposition.h"
 
 class Browser;
 class SkBitmap;
@@ -56,6 +57,11 @@ class BackForwardMenuModel : public menus::MenuModel {
   virtual MenuModel* GetSubmenuModelAt(int index) const;
   virtual void HighlightChangedTo(int index);
   virtual void ActivatedAt(int index);
+
+  // Navigates to the corresponding history item, opening it in a new tab
+  // if necessary.
+  void ActivatedAtWithDisposition(int index,
+                                  WindowOpenDisposition disposition);
   virtual void MenuWillShow();
 
   // Is the item at |index| a separator?

chrome/browser/cocoa/back_forward_menu_controller.mm
@@ -9,6 +9,7 @@
 #include "base/sys_string_conversions.h"
 #include "chrome/browser/back_forward_menu_model.h"
 #import "chrome/browser/cocoa/delayedmenu_button.h"
+#import "chrome/browser/cocoa/event_utils.h"
 #include "skia/ext/skia_utils_mac.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 
@@ -93,7 +94,9 @@ using gfx::SkBitmapToNSImage;
 - (void)executeMenuItem:(id)sender {
   DCHECK([sender isKindOfClass:[NSMenuItem class]]);
   int menuID = [sender tag];
-  model_->ActivatedAt(menuID);
+  model_->ActivatedAtWithDisposition(
+      menuID,
+      event_utils::WindowOpenDispositionFromNSEvent([NSApp currentEvent]));
 }
 
 @end  // @implementation BackForwardMenuController

commit 006b1918187f7d815e7f7a59d5ce1b1723a66193

Fix a regression appearing in r41875 where navigating back from an interstitial page wouldn't work.
Also, factor out a method WindowDispositionShouldOpenNewTab that's used to decide if a given window disposition opens in a new tab or the current tab.

BUG=39248
TEST=SSLUITest.TestHTTPSExpiredCertAndGoBackViaButton, SSLUITest.TestHTTPSExpiredCertAndGoBackViaMenu, manual test: Go to www.google.com, then to https://ebay.com to show the HTTPS interstitial, then navigate back using the toolbar.

Review URL: http://codereview.chromium.org/1406001

git-svn-id: http://src.chromium.org/svn/trunk/src@42974 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

chrome/browser/browser.cc
@@ -893,11 +893,16 @@ void Browser::UpdateCommandsForFullscreenMode(bool is_fullscreen) {
 ///////////////////////////////////////////////////////////////////////////////
 // Browser, Assorted browser commands:
 
+bool Browser::ShouldOpenNewTabForWindowDisposition(
+    WindowOpenDisposition disposition) {
+  return (disposition == NEW_FOREGROUND_TAB ||
+          disposition == NEW_BACKGROUND_TAB);
+}
+
 NavigationController& Browser::GetOrCloneNavigationControllerForDisposition(
        WindowOpenDisposition disposition) {
   TabContents* current_tab = GetSelectedTabContents();
-  if (disposition == NEW_FOREGROUND_TAB ||
-      disposition == NEW_BACKGROUND_TAB) {
+  if (ShouldOpenNewTabForWindowDisposition(disposition)) {
     TabContents* cloned = current_tab->Clone();
     tabstrip_model_.AddTabContents(cloned, -1, false,
                                    PageTransition::LINK,
@@ -916,11 +921,13 @@ void Browser::GoBack(WindowOpenDisposition disposition) {
   if (current_tab->controller().CanGoBack()) {
     NavigationController& controller =
         GetOrCloneNavigationControllerForDisposition(disposition);
-    // The interstitial won't be copied to the new tab, so we don't need to
-    // go back.
-    if (!current_tab->interstitial_page()) {
-      controller.GoBack();
+    // If we are on an interstitial page and clone the tab, it won't be copied
+    // to the new tab, so we don't need to go back.
+    if (current_tab->interstitial_page() &&
+        ShouldOpenNewTabForWindowDisposition(disposition)) {
+      return;
     }
+    controller.GoBack();
   }
 }
 

chrome/browser/browser.h
@@ -821,6 +821,9 @@ class Browser : public TabStripModelDelegate,
   // Shared code between Reload() and ReloadAll().
   void ReloadInternal(bool ignore_cache);
 
+  // Return true if the window dispositions means opening a new tab.
+  bool ShouldOpenNewTabForWindowDisposition(WindowOpenDisposition disposition);
+
   // Depending on the disposition, return the |NavigationController| for the
   // current tab or clone the current tab and return its |NavigationController|.
   NavigationController& GetOrCloneNavigationControllerForDisposition(

chrome/browser/ssl/ssl_browser_tests.cc
@@ -220,8 +220,36 @@ IN_PROC_BROWSER_TEST_F(SSLUITest, TestHTTPSExpiredCertAndDontProceed) {
   CheckUnauthenticatedState(tab);
 }
 
+// Visits a page with https error and then goes back using Browser::GoBack.
+IN_PROC_BROWSER_TEST_F(SSLUITest, TestHTTPSExpiredCertAndGoBackViaButton) {
+  scoped_refptr<HTTPTestServer> http_server = PlainServer();
+  ASSERT_TRUE(http_server.get() != NULL);
+  scoped_refptr<HTTPSTestServer> bad_https_server = BadCertServer();
+  ASSERT_TRUE(bad_https_server.get() != NULL);
+
+  // First navigate to an HTTP page.
+  ui_test_utils::NavigateToURL(browser(), http_server->TestServerPageW(
+      L"files/ssl/google.html"));
+  TabContents* tab = browser()->GetSelectedTabContents();
+  NavigationEntry* entry = tab->controller().GetActiveEntry();
+  ASSERT_TRUE(entry);
+
+  // Now go to a bad HTTPS page that shows an interstitial.
+  ui_test_utils::NavigateToURL(browser(),
+      bad_https_server->TestServerPageW(L"files/ssl/google.html"));
+  CheckAuthenticationBrokenState(tab, net::CERT_STATUS_DATE_INVALID,
+                                 true);  // Interstitial showing
+
+  // Simulate user clicking on back button (crbug.com/39248).
+  browser()->GoBack(CURRENT_TAB);
+
+  // We should be back at the original good page.
+  EXPECT_FALSE(browser()->GetSelectedTabContents()->interstitial_page());
+  CheckUnauthenticatedState(tab);
+}
+
 // Visits a page with https error and then goes back using GoToOffset.
-IN_PROC_BROWSER_TEST_F(SSLUITest, TestHTTPSExpiredCertAndGoBack) {
+IN_PROC_BROWSER_TEST_F(SSLUITest, TestHTTPSExpiredCertAndGoBackViaMenu) {
   scoped_refptr<HTTPTestServer> http_server = PlainServer();
   ASSERT_TRUE(http_server.get() != NULL);
   scoped_refptr<HTTPSTestServer> bad_https_server = BadCertServer();
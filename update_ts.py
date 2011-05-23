a09c40ef41d03925fff3d91eac689c6489678609
Wei James <james.wei@intel.com>
2011-05-23 16:54:42 +0800

    Utils: update update_ts.py to include ui/base/strings/app_strings.grd

--- update_ts.py
+++ update_ts.py
@@ -14,7 +14,8 @@ from xml.dom import minidom
 base_commit = "16e43edc265780a01d2b94cba1aefcdad63661bd"
 
 grd_files = ['chrome/app/generated_resources.grd',
-             'chrome/app/chromium_strings.grd']
+             'chrome/app/chromium_strings.grd',
+             'ui/base/strings/app_strings.grd']
 
 ts_dir = './ts'
 po_dir = './po'
